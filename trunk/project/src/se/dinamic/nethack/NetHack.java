fe5a395be3d714c62ae3cf04593e346b82b53cb4
henrik.4e@gmail.com <henrik.4e@gmail.com@24301e39-3568-6c37-c5f9-436d4350a34e>
2010-05-09 22:45:08 +0000

    Removed unused code... Added a thread in native code to run the nethack...

--- trunk/project/src/se/dinamic/nethack/NetHack.java
+++ trunk/project/src/se/dinamic/nethack/NetHack.java
@@ -10,63 +10,37 @@ import android.view.WindowManager;
 
 public class NetHack extends Activity 
 {
-	private NetHackThread  _nethack;
+    private NetHackEngine  _nethack;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-	
-	getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-	getWindow().addFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN ); 	    
-	
-        _nethack=new NetHackThread(this);
-	setContentView( _nethack.getEngine().getView());
+    
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags( WindowManager.LayoutParams.FLAG_FULLSCREEN ); 	    
+    
+        _nethack = new NetHackEngine( this );
+        setContentView( _nethack.getView() );
         
-	Log.v("NetHack","Running nethack thread...");
-	_nethack.run();
+        _nethack.start();
     }
     
-	@Override
-	public void onDestroy( )
-	{
-		super.onDestroy( );
-	}
-
-	@Override
-	protected void onResume( ) {
-		super.onResume( );
-		_nethack.getEngine().getView().onResume( );
-	}
-
-	@Override
-	protected void onPause( ) {
-		super.onPause( );
-		_nethack.getEngine().getView().onPause( );
-	}
-    
-    private class NetHackThread extends Thread {
-        private NetHackEngine _engine;
-        private Activity _activity;
-        public NetHackThread(Activity activity) {
-            _activity=activity;
-	    _engine = new NetHackEngine(_activity);
-	   
-        }
-        public NetHackEngine getEngine() {
-		return _engine;
-	}
-	
-        public void run() {
-            if( !_engine.run() ) {
-                Toast toast = Toast.makeText(_activity, "Failed to initialize nethack with reason:\n "+_engine.error(), Toast.LENGTH_LONG);
-                toast.show();
-              //  _activity.finish();
-            }
-            // Nethack game has exited..
-	    
-	    // Let's clean up and exit application...
-        }
+    @Override
+    public void onDestroy( )
+    {
+        super.onDestroy( );
     }
-    
+
+    @Override
+    protected void onResume( ) {
+        super.onResume( );
+        _nethack.onResume( );
+    }
+
+    @Override
+    protected void onPause( ) {
+        super.onPause( );
+        _nethack.onPause( );
+    }
 }
