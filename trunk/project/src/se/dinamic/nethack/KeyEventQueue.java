46d46fa6c5bf68894e6e26b487a34b665a6c164b
henrik.4e@gmail.com <henrik.4e@gmail.com@24301e39-3568-6c37-c5f9-436d4350a34e>
2010-05-10 16:28:02 +0000

    Initial keymappings for navigation DPAD up/down/left/right support and default "hjkl." plus metabit...

--- trunk/project/src/se/dinamic/nethack/KeyEventQueue.java
+++ trunk/project/src/se/dinamic/nethack/KeyEventQueue.java
@@ -2,9 +2,11 @@ package se.dinamic.nethack;
 import android.util.Log;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.lang.InterruptedException;
+import android.view.KeyEvent;
 
 public class KeyEventQueue {
     private LinkedBlockingQueue<Integer> _queue;
+    public final static int NETHACK_META_BIT = 128;
     
     public KeyEventQueue() {
         _queue=new LinkedBlockingQueue<Integer>(100);
@@ -22,9 +24,48 @@ public class KeyEventQueue {
     
     /** Add a key to the queue */
     public void addKey(int keysym) {
-        Log.v("KeyEventQueue.addKey","Added key "+keysym);
+        int key=0;
         try {
-            _queue.put(keysym);
+            switch(keysym) {
+                /*
+                 * NAVIGATION KEY MAPPING
+                 */
+                case KeyEvent.KEYCODE_K:
+                case KeyEvent.KEYCODE_DPAD_UP:
+                    key = 'k' | ((keysym&KeyEvent.META_SHIFT_LEFT_ON)!=0?NETHACK_META_BIT:0);
+                break;
+                
+                case KeyEvent.KEYCODE_J:
+                case KeyEvent.KEYCODE_DPAD_DOWN:
+                    key = 'j' | ((keysym&KeyEvent.META_SHIFT_LEFT_ON)!=0?NETHACK_META_BIT:0);
+                break;
+                
+                case KeyEvent.KEYCODE_H:
+                case KeyEvent.KEYCODE_DPAD_LEFT:
+                    key = 'h' | ((keysym&KeyEvent.META_SHIFT_LEFT_ON)!=0?NETHACK_META_BIT:0);
+                break;
+                
+                case KeyEvent.KEYCODE_L:
+                case KeyEvent.KEYCODE_DPAD_RIGHT:
+                    key = 'l' | ((keysym&KeyEvent.META_SHIFT_LEFT_ON)!=0?NETHACK_META_BIT:0);
+                break;
+                
+                case KeyEvent.KEYCODE_PERIOD:
+                case KeyEvent.KEYCODE_DPAD_CENTER:
+                    key = '.' | ((keysym&KeyEvent.META_SHIFT_LEFT_ON)!=0?NETHACK_META_BIT:0);
+                break;
+                
+                case KeyEvent.KEYCODE_Q:
+                case KeyEvent.KEYCODE_BACK:
+                    key = 'q' | ((keysym&KeyEvent.META_SHIFT_LEFT_ON)!=0?NETHACK_META_BIT:0);
+                break;
+                
+            }
+            
+            // Add key to queue
+            if(key!=0)
+                _queue.put(key);
+            
         } catch (InterruptedException e) {
         }
     }
