5dc1cb1fd6261fe9e6c76ac49d8c9c7b195464d7
henrik.4E@gmail.com <henrik.4E@gmail.com@24301e39-3568-6c37-c5f9-436d4350a34e>
2010-05-10 12:25:10 +0000

    Initial rendering of tiles of map (untextured)

--- trunk/project/src/se/dinamic/nethack/NetHackMap.java
+++ trunk/project/src/se/dinamic/nethack/NetHackMap.java
@@ -7,13 +7,21 @@ class NetHackMap {
 	/** This should be replaced with a more sophisticated type than int... like NetHackMapObject */
 	private int _map[][];
 	
-	public void NetHackMap () {
+	protected int _playerX,_playerY;
+	
+	public NetHackMap () {
 		_map=new int[SIZE][SIZE];
 	}
+	
 	public int get(int x,int y) {
 		return _map[x][y];
 	}
+	
 	public void handleGlyph(int x, int y,int glyph) {
+		if( glyph>=319 && glyph<=340 ) { // we got a player glyph
+			_playerX=x;
+			_playerY=y;
+		}
 		_map[x][y]=glyph;
 	}
 }
--- trunk/project/src/se/dinamic/nethack/NetHackMapRenderer.java
+++ trunk/project/src/se/dinamic/nethack/NetHackMapRenderer.java
@@ -1,12 +1,50 @@
 package se.dinamic.nethack;
+
+import java.nio.ShortBuffer;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGL10;
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 public class NetHackMapRenderer extends NetHackMap implements NetHackRenderer{
+	
+	private float _tile_v[] = {
+	      -0.5f,  0.5f, 0.0f,  // 0, Top Left
+	      -0.5f, -0.5f, 0.0f,  // 1, Bottom Left
+	       0.5f, -0.5f, 0.0f,  // 2, Bottom Right
+	       0.5f,  0.5f, 0.0f,  // 3, Top Right
+	};
+	
+	private short[] _tile_i = { 0, 1, 2, 0, 2, 3 };
+	
+	public FloatBuffer _tile_varr;
+	public ShortBuffer _tile_iarr;
+    
+	public NetHackMapRenderer() {
+		// Setup tile object
+		ByteBuffer vbb = ByteBuffer.allocateDirect(_tile_v.length * 4);
+		vbb.order(ByteOrder.nativeOrder());
+		_tile_varr = vbb.asFloatBuffer();
+		_tile_varr.put(_tile_v);
+		_tile_varr.position(0);
+        
+		ByteBuffer ibb = ByteBuffer.allocateDirect(_tile_i.length * 2);
+		ibb.order(ByteOrder.nativeOrder());
+		_tile_iarr = ibb.asShortBuffer();
+		_tile_iarr.put(_tile_i);
+		_tile_iarr.position(0);
+        
+	}
+	
 	public void render(GL10 gl) {
+		
+		// Translate map so its centered on player
+		gl.glTranslatef(-_playerX,-_playerY,0);
+		
 		gl.glPushMatrix();
-		gl.glRotatef(-90.0f,1.0f,0.0f,0.0f);
 		// Run thru all entries in map and render tile
 		for(int y=0;y<NetHackMap.SIZE;y++) {
 			// Translate into y 
@@ -17,6 +55,8 @@ public class NetHackMapRenderer extends NetHackMap implements NetHackRenderer{
 				int glyph=get(x,y);
 				if(  glyph != 0 ) {
 					// Render the texturemapped tile
+					gl.glVertexPointer(3, gl.GL_FLOAT, 0, _tile_varr);
+					gl.glDrawElements(GL10.GL_TRIANGLES, _tile_i.length, GL10.GL_UNSIGNED_SHORT, _tile_iarr);
 				}
 			}
 			gl.glTranslatef(-NetHackMap.SIZE,0,0);
